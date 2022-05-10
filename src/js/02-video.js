a2638b206480a75400e0c0e61c0156d585014127
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-10 17:50:09 +0300

    update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const player = new Player(refs.iframe);
+const refs = {
+  iframe: document.querySelector('#vimeo-player'),
+};
+
+//getting and adding stop-time in local Storage
+const onTimeupdate = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+//run since last view
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
