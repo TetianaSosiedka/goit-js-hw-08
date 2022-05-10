5e8697a6d0408bfadd820a49d2baf3d48f1475c2
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-10 15:17:25 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const refs = {
+  divGallery: document.querySelector('.gallery'),
+};
+//===============================================
+function markupGallery() {
+  galleryItems.map(galleryItem => {
+    refs.divGallery.insertAdjacentHTML('beforeend', markupElementGallery(galleryItem));
+  });
+}
+function markupElementGallery({ preview, original, description }) {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+}
+//===============================================
+function oupenModalWindow() {
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+}
 
-console.log(galleryItems);
+markupGallery(); //Creating and rendering gallery
+oupenModalWindow(); //Oupen modal window with img
