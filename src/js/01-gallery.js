dab1f72fb2aa517b3b34c4cc0faa65e0174efa6e
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-10 15:55:14 +0300

    template for gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
+import ElementGalleryTemp from '../templates/element-gallery.hbs';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
@@ -9,13 +11,8 @@ const refs = {
 //===============================================
 function markupGallery() {
   galleryItems.map(galleryItem => {
-    refs.divGallery.insertAdjacentHTML('beforeend', markupElementGallery(galleryItem));
+    refs.divGallery.insertAdjacentHTML('beforeend', ElementGalleryTemp(galleryItem));
   });
-}
-function markupElementGallery({ preview, original, description }) {
-  return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`;
 }
 //===============================================
 function oupenModalWindow() {
