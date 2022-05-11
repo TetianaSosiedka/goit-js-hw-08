52b17ba600edcb557becf4f70d48e1bf1c24b365
TetianaSosiedka <tasha.sosedka@gmail.com>
2022-05-11 16:33:25 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
+import throttle from 'lodash.throttle';
+//---------------------------------------------------------------
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  formMail: document.querySelector('.feedback-form input'),
+  formMessage: document.querySelector('.feedback-form textarea'),
+};
+let formDates = {
+  mail: '',
+  message: '',
+};
+//---------------------------------------------------------------
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
+fillForms(); //fill out the form Local Storage
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log('You entered:', formDates);
+}
+function onFormInput(event) {
+  if (event.target.nodeName === 'INPUT') {
+    formDates.mail = event.target.value;
+  }
+  if (event.target.nodeName === 'TEXTAREA') {
+    formDates.message = event.target.value;
+  }
+  addFormDatesInLocalStorade();
+}
+function addFormDatesInLocalStorade() {
+  localStorage.setItem('feedback-form-state', JSON.stringify(formDates));
+}
+
+function fillForms() {
+  const saveDates = localStorage.getItem('feedback-form-state');
+  if (saveDates) {
+    const { mail, message } = JSON.parse(saveDates);
+    if (mail) {
+      refs.form.querySelector('input[name="email"]').value = mail;
+      formDates.mail = mail;
+    }
+    if (message) {
+      refs.form.querySelector('textarea').value = message;
+      formDates.message = message;
+    }
+  }
+}
